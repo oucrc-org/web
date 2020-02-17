c3e82914fe5a225bf031ba13c075c7fdbdb67334
keita <banira0428@gmail.com>
2020-02-17 12:20:03 +0900

    コンテンツ用のコンポーネント作成

--- src/components/Contents.vue
+++ src/components/Contents.vue
@@ -1,15 +1,45 @@
 <template>
-    <ul uk-accordion class="uk-list uk-padding-small">
-        <li class="uk-padding-small" v-for="n in 5" v-bind:key="n">
-            <a class="uk-accordion-title" href="#">新入生の方へ</a>
-            <div class="uk-accordion-content">中身</div>
+    <ul uk-accordion="multiple:true" class="uk-list uk-padding-small">
+        <li v-bind:class="(index === 0) ? 'uk-padding-small uk-open' : 'uk-padding-small'" v-for="(content,index) in contents" v-bind:key="index" >
+            <a class="uk-accordion-title" href="#">{{content.title}}</a>
+            <div class="uk-accordion-content">{{content.description}}</div>
         </li>
     </ul>
 </template>
 
 <script>
     export default {
-        name: "Contents.vue"
+        name: "Contents.vue",
+        data() {
+            return {
+                contents: [
+                    {
+                        title: "新入生の方へ",
+                        description: "Welcome!"
+                    },
+                    {
+                        title: "活動内容",
+                        description: "Welcome!"
+                    },
+                    {
+                        title: "活動場所",
+                        description: "Welcome!"
+                    },
+                    {
+                        title: "活動時間",
+                        description: "Welcome!"
+                    },
+                    {
+                        title: "設備",
+                        description: "Welcome!"
+                    },
+                    {
+                        title: "部員一覧",
+                        description: "Welcome!"
+                    },
+                ]
+            }
+        }
     }
 </script>
 
