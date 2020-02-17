2534bcedbf5997213f46a1a27f3904618872656c
keita <banira0428@gmail.com>
2020-02-17 11:04:20 +0900

    uikit の導入

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,19 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
+    <Header/>
     <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import Header from './components/Header.vue'
 
 export default {
   name: 'App',
   components: {
+    Header,
     HelloWorld
   }
 }
