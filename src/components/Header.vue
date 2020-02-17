c699a388b6955583191063fe0ee02bf862385274
keita <banira0428@gmail.com>
2020-02-17 20:46:50 +0900

    ヘッダ固定

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,6 +1,6 @@
 <template>
-    <header class="uk-sticky uk-padding-small uk-heading-divider">
-            <img width="160" height="28" src="../assets/img/logo.png" alt="サークルのロゴ画像" >
+    <header uk-sticky="offset: 0; bottom: #top" class="uk-padding-small uk-heading-divider uk-background-default">
+            <a href="#"><img width="160" height="28" src="../assets/img/logo.png" alt="サークルのロゴ画像" ></a>
     </header>
 </template>
 
