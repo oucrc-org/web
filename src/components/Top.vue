63c1e3bfdeebcf935bddad2b5b03f525e3680514
matsufriends <pikapikan0526@gmail.com>
2020-07-25 23:47:06 +0900

    Top画像をpublicフォルダへ移動，v-forで表示

--- src/components/Top.vue
+++ src/components/Top.vue
@@ -2,24 +2,21 @@
     <div class="uk-position-relative" uk-slideshow="animation: fade">
 
         <ul class="uk-slideshow-items">
-            <li>
-                <img src="../assets/img/top-1.png" alt="" uk-cover>
+
+            <li v-for="num in 3" :key="num">
+                 <img v-bind:src="'/img/top-' + num +'.png'" alt="uk-cover">
             </li>
-            <li>
-                <img src="../assets/img/top-2.png" alt="" uk-cover>
-            </li>
-            <li>
-                <img src="../assets/img/top-3.png" alt="" uk-cover>
-            </li>
+
         </ul>
 
         <div class="uk-position-bottom-center uk-position-small">
             <ul class="uk-thumbnav">
-                <li uk-slideshow-item="0"><a href="#"><img src="../assets/img/top-1.png" width="64" alt="トップ画像"></a></li>
-                <li uk-slideshow-item="1"><a href="#"><img src="../assets/img/top-2.png" width="64" alt=""></a></li>
-                <li uk-slideshow-item="2"><a href="#"><img src="../assets/img/top-3.png" width="64" alt=""></a></li>
+                <li uk-slideshow-item="0"><a href="#"><img src="../../public/img/top-1.png" width="64" alt="トップ画像"></a></li>
+                <li uk-slideshow-item="1"><a href="#"><img src="../../public/img/top-2.png" width="64" alt=""></a></li>
+                <li uk-slideshow-item="2"><a href="#"><img src="../../public/img/top-3.png" width="64" alt=""></a></li>
             </ul>
         </div>
+
     </div>
 </template>
 
