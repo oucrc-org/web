30cb9f9164abbb646f8a6008d88beaa3d6177f2b
AAAR Salmon <TenzikuJohn@gmail.com>
2020-11-01 02:39:02 +0900

    update: お知らせの更新

--- src/components/Notice.vue
+++ src/components/Notice.vue
@@ -1,14 +1,20 @@
 <template>
     <div>
         <h1 class="uk-h4 uk-padding-small">
-            <span uk-icon="info" class="uk-margin-small-right"></span>お知らせ
+            <span uk-icon="bell" class="uk-margin-small-right"></span>お知らせ
         </h1>
 
-        <div class="uk-padding-small">
+        <div id="notice" class="uk-padding-small">
             <ul class="uk-list uk-margin">
+            <!--
+                特段の注意が必要な事項には
+                <span uk-icon="warning" class="uk-text-danger uk-margin-small-right"></span>
+                一般的な情報には
+                <span uk-icon="info" class="uk-text-primary uk-margin-small-right"></span>
+            -->
                 <li>
-                    <span uk-icon="warning" class="uk-text-danger uk-margin-small-right"></span>
-                    新型感染症の影響で現在部室への立ち入りを制限しています。<a href="https://hackmd.io/@leN8onDkQoKpjiihHJ5eAA/H1Mpzl3YU">【新入生向けの案内はこちら】</a>
+                    <span uk-icon="info" class="uk-text-primary uk-margin-small-right"></span>
+                    部室での活動を再開しました。見学の際は気軽にお越しください。<a href="https://hackmd.io/@leN8onDkQoKpjiihHJ5eAA/H1Mpzl3YU">【新入生向けの案内はこちら】</a>
                 </li>
             </ul>
         </div>
